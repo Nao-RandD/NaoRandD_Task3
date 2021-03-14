29146ba1b6467da7ec02d64d1620148a2c0a4590
akio0911 <akio0911@gmail.com>
2021-03-14 11:38:48 +0900

    feedback

--- AppDojoTask3/ViewController.swift
+++ AppDojoTask3/ViewController.swift
@@ -9,41 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	@IBOutlet private weak var leftField: UITextField!
-	@IBOutlet private weak var rightField: UITextField!
+	@IBOutlet private weak var leftTextField: UITextField!
+	@IBOutlet private weak var rightTextField: UITextField!
 	@IBOutlet private weak var leftSwitch: UISwitch!
 	@IBOutlet private weak var rightSwitch: UISwitch!
-	@IBOutlet private weak var leftText: UILabel!
-	@IBOutlet private weak var rightText: UILabel!
-	@IBOutlet private weak var resultText: UILabel!
+	@IBOutlet private weak var leftLabel: UILabel!
+	@IBOutlet private weak var rightLabel: UILabel!
+	@IBOutlet private weak var resultLabel: UILabel!
 
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// 数字のみの入力となるように設定
-		leftField.keyboardType = .numberPad
-		rightField.keyboardType = .numberPad
+		leftTextField.keyboardType = .numberPad
+		rightTextField.keyboardType = .numberPad
 	}
 
 	@IBAction private func tapButton(_ sender: Any) {
-		var value1 = Int(leftField.text!) ?? 0
-		var value2 = Int(rightField.text!) ?? 0
+		let leftValue = Int(leftTextField.text!) ?? 0
+		let rightValue = Int(rightTextField.text!) ?? 0
 
-		guard value1 != 0 || value2 != 0 else {
-			resultText.text = "数字を入力してください！"
+		guard leftValue != 0 && rightValue != 0 else {
+			resultLabel.text = "数字を入力してください！"
 			return
 		}
 
-		if(leftSwitch.isOn){
-			value1 *= -1
-		}
-		if(rightSwitch.isOn){
-			value2 *= -1
-		}
+        let signedLeftValue: Int
+        if leftSwitch.isOn {
+            signedLeftValue = -leftValue
+        } else {
+            signedLeftValue = leftValue
+        }
 
-		leftText.text = String(value1)
-		rightText.text = String(value2)
-		resultText.text = String(value1 + value2)
+        let signedRightValue: Int
+        if rightSwitch.isOn {
+            signedRightValue = -rightValue
+        } else {
+            signedRightValue = rightValue
+        }
+
+		leftLabel.text = String(signedLeftValue)
+		rightLabel.text = String(signedRightValue)
+		resultLabel.text = String(signedLeftValue + signedRightValue)
 	}
 }
 
